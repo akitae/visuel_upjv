56f520062446a65582efde7ee070fad2ea02670c
Noemie <nleitao@sedonaintra.lan>
2018-05-05 21:32:20 +0200

    Add sub-content

---
+++ sites/default/files/php/twig/5ae7185d1de56_block--system-branding-bl_lHi6IxaUmfjG1rqzWZc3DdOFN/G1EC9V1imgVwAfFTgU6eRAqRcTAhTs7nBUlR6_TN-2k.php
@@ -0,0 +1,134 @@
+<?php
+
+/* themes/upjv/templates/block/block--system-branding-block.html.twig */
+class __TwigTemplate_47f1d430c9bdbad605ce452da60945887884131dd8cfa71971b464da2ceb5f6e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("block--bare.html.twig", "themes/upjv/templates/block/block--system-branding-block.html.twig", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "block--bare.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 23);
+        $filters = array("t" => 24);
+        $functions = array("dump" => 21, "path" => 24);
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if'),
+                array('t'),
+                array('dump', 'path')
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 18
+    public function block_content($context, array $blocks = array())
+    {
+        // line 19
+        echo "
+    <div id=\"block-upjv-branding\">
+        ";
+        // line 21
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_var_dump($this->env, $context, ($context["page"] ?? null)), "html", null, true));
+        echo "
+        TEST
+        ";
+        // line 23
+        if (($context["site_logo"] ?? null)) {
+            // line 24
+            echo "            <a class=\"logo navbar-btn pull-left\" href=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->env->getExtension('Drupal\Core\Template\TwigExtension')->getPath("<front>")));
+            echo "\" title=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Home")));
+            echo "\" rel=\"home\">
+                <img src=\"";
+            // line 25
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["site_logo"] ?? null), "html", null, true));
+            echo "\" alt=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Home")));
+            echo "\" />
+            </a>
+        ";
+        }
+        // line 28
+        echo "        ";
+        if (($context["site_name"] ?? null)) {
+            // line 29
+            echo "            <a class=\"name navbar-brand\" href=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($this->env->getExtension('Drupal\Core\Template\TwigExtension')->getPath("<front>")));
+            echo "\" title=\"";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Home")));
+            echo "\" rel=\"home\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["site_name"] ?? null), "html", null, true));
+            echo "</a>
+        ";
+        }
+        // line 31
+        echo "        ";
+        if (($context["site_slogan"] ?? null)) {
+            // line 32
+            echo "            <p class=\"navbar-text\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["site_slogan"] ?? null), "html", null, true));
+            echo "</p>
+        ";
+        }
+        // line 34
+        echo "    </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/upjv/templates/block/block--system-branding-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  103 => 34,  97 => 32,  94 => 31,  84 => 29,  81 => 28,  73 => 25,  66 => 24,  64 => 23,  59 => 21,  55 => 19,  52 => 18,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/upjv/templates/block/block--system-branding-block.html.twig", "/var/www/drupal/themes/upjv/templates/block/block--system-branding-block.html.twig");
+    }
+}
